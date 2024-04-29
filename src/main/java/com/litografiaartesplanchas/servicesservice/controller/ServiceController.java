d1b371e1f9ce9a53832fdb9a8b2713bc3fbe4879
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-04-28 20:01:38 -0500

    LAP-131 - Implements endpoint to get all services and all service types in database

--- src/main/java/com/litografiaartesplanchas/servicesservice/controller/ServiceController.java
+++ src/main/java/com/litografiaartesplanchas/servicesservice/controller/ServiceController.java
@@ -1,15 +1,26 @@
 package com.litografiaartesplanchas.servicesservice.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.litografiaartesplanchas.servicesservice.model.ServiceModel;
 import com.litografiaartesplanchas.servicesservice.model.TypeService;
 import com.litografiaartesplanchas.servicesservice.service.ServiceService;
+import com.litografiaartesplanchas.servicesservice.utils.Response;
+import com.litografiaartesplanchas.servicesservice.utils.ResponseBody;
+import com.litografiaartesplanchas.servicesservice.utils.errors.ErrorHandlerResponse;
+import com.litografiaartesplanchas.servicesservice.utils.errors.NotFoundException;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("api/v1/service")
@@ -17,23 +28,36 @@ public class ServiceController {
 	@Autowired
 	private ServiceService serviceService;
 	
-	/**
-	 * This Java function uses a GET mapping to retrieve a list of ServiceModel objects.
-	 * 
-	 * @return A list of ServiceModel objects is being returned.
-	 */
 	@GetMapping("/")
-	public List<ServiceModel> getAll(){
-		return serviceService.getAll();
+	public ResponseEntity<ResponseBody> getAll(){
+		List<ServiceModel> data = serviceService.getAll();
+		return Response.ok(data);
 	}
 
-	/**
-	 * This Java function returns a list of TypeService objects by calling a method from a service class.
-	 * 
-	 * @return A list of TypeService objects is being returned.
-	 */
+	/*
+	@GetMapping(value = "/{id}")
+	@ApiResponses({
+        @ApiResponse(responseCode = "200"),
+        @ApiResponse(description = "Not found", responseCode = "404")
+    })
+	public ResponseEntity<ResponseBody> getById(@PathVariable long id){
+		try {
+			Optional<ServiceModel> optionalService = serviceService.getServiceById(id);
+			if(optionalService.isEmpty()) {
+				throw new NotFoundException("Client not Found");
+			}
+			ServiceModel service = optionalService.get();
+			ArrayList<ServiceModel> data = new ArrayList<ServiceModel>();
+			data.add(service);
+			return Response.ok(data);
+		}catch(Exception e) {
+			return ErrorHandlerResponse.handleException(e);
+		}
+	}*/
+	
 	@GetMapping("/types")
-	public List<TypeService> getAllTypes(){
-		return serviceService.getTypesService();
+	public ResponseEntity<ResponseBody> getAllTypes(){
+		List<TypeService> data = serviceService.getTypesService();
+		return Response.ok(data);
 	}
 }
