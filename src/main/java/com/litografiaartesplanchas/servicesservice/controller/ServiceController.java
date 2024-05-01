99dd6030237252b9c9c28e62b2676b7b724a3ab0
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-05-01 12:14:39 -0500

    LAP-134 - Refactor logic of controller and service

--- src/main/java/com/litografiaartesplanchas/servicesservice/controller/ServiceController.java
+++ src/main/java/com/litografiaartesplanchas/servicesservice/controller/ServiceController.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,69 +30,64 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 @RestController
 @RequestMapping("api/v1/service")
 public class ServiceController {
-	@Autowired
-	private ServiceService serviceService;
-	
-	@GetMapping("/")
-	public ResponseEntity<ResponseBody> getAll(){
-		List<ServiceModel> data = serviceService.getAll();
-		return Response.ok(data);
-	}
+    @Autowired
+    private ServiceService serviceService;
+    
+    @GetMapping("/")
+    public ResponseEntity<ResponseBody> getAll(){
+        List<ServiceModel> data = serviceService.getAll();
+        return Response.ok(data);
+    }
 
-	@GetMapping(value = "/{id}")
-	@ApiResponses({
+    @GetMapping(value = "/{id}")
+    @ApiResponses({
         @ApiResponse(responseCode = "200"),
         @ApiResponse(description = "Not found", responseCode = "404")
     })
-	public ResponseEntity<ResponseBody> getById(@PathVariable long id){
-		try {
-			Optional<ServiceModel> optionalService = serviceService.getServiceById(id);
-			if(optionalService.isEmpty()) {
-				throw new NotFoundException("Client not Found");
-			}
-			ServiceModel service = optionalService.get();
-			ArrayList<ServiceModel> data = new ArrayList<ServiceModel>();
-			data.add(service);
-			return Response.ok(data);
-		}catch(Exception e) {
-			return ErrorHandlerResponse.handleException(e);
-		}
-	}
-	
-	@GetMapping("/types")
-	public ResponseEntity<ResponseBody> getAllTypes(){
-		List<TypeService> data = serviceService.getTypesService();
-		return Response.ok(data);
-	}
-	
-	@GetMapping("/types/{id}")
-	@ApiResponses({
+    public ResponseEntity<ResponseBody> getById(@PathVariable long id){
+        try {
+            ServiceModel service = serviceService.getServiceById(id);
+            ArrayList<ServiceModel> data = new ArrayList<ServiceModel>();
+            data.add(service);
+            return Response.ok(data);
+        }catch(Exception e) {
+            return ErrorHandlerResponse.handleException(e);
+        }
+    }
+    
+    @GetMapping("/types")
+    public ResponseEntity<ResponseBody> getAllTypes(){
+        List<TypeService> data = serviceService.getTypesService();
+        return Response.ok(data);
+    }
+    
+    @GetMapping("/types/{id}")
+    @ApiResponses({
         @ApiResponse(responseCode = "200"),
         @ApiResponse(description = "Not found", responseCode = "404")
     })
-	public ResponseEntity<ResponseBody> getByType(@PathVariable int id){
-		try {
-			List<ServiceModel> data = serviceService.getServicesByType(id);
-			return Response.ok(data);
-		}catch(Exception e) {
-			return ErrorHandlerResponse.handleException(e);
-		}
-	}
-	
-	@PostMapping("/create")
-	@ApiResponses({
+    public ResponseEntity<ResponseBody> getByType(@PathVariable int id){
+        try {
+            List<ServiceModel> data = serviceService.getServicesByType(id);
+            return Response.ok(data);
+        }catch(Exception e) {
+            return ErrorHandlerResponse.handleException(e);
+        }
+    }
+    
+    @PostMapping("/create")
+    @ApiResponses({
         @ApiResponse(responseCode = "200"),
         @ApiResponse(description = "Conflict", responseCode = "409")
     })
-	public ResponseEntity<ResponseBody> createService(@RequestBody ServiceModel service){
-		try {
-			serviceService.createService(service);
-			return Response.ok();
-		}catch(Exception e) {
-			System.out.println(e.getMessage());
-			return ErrorHandlerResponse.handleException(e);
-		}
-	}
-	
-	
+    public ResponseEntity<ResponseBody> createService(@RequestBody ServiceModel service){
+        try {
+            serviceService.createService(service);
+            return Response.ok();
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+            return ErrorHandlerResponse.handleException(e);
+        }
+    }
+    
 }
--- src/main/java/com/litografiaartesplanchas/servicesservice/service/ServiceService.java
+++ src/main/java/com/litografiaartesplanchas/servicesservice/service/ServiceService.java
@@ -2,7 +2,6 @@ package com.litografiaartesplanchas.servicesservice.service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,57 +20,61 @@ import com.litografiaartesplanchas.servicesservice.utils.errors.NotFoundExceptio
 
 @Service
 public class ServiceService {
-	@Autowired
-	private IServiceRepository serviceRepository;
-	@Autowired
-	private ITypeServiceRepository typeServiceRepository;
-	@Autowired
-	private IServiceMaterialRepository serviceHasMaterialRepository;
-	@Autowired
-	private IMaterialRepository materialRepository;
-	@Autowired
-	private IEmployeeRepository employeeRepository;
-	
-	public List<ServiceModel> getAll(){
-		return serviceRepository.findAll();
-	}
+    @Autowired
+    private IServiceRepository serviceRepository;
+    @Autowired
+    private ITypeServiceRepository typeServiceRepository;
+    @Autowired
+    private IServiceMaterialRepository serviceHasMaterialRepository;
+    @Autowired
+    private IMaterialRepository materialRepository;
+    @Autowired
+    private IEmployeeRepository employeeRepository;
+    
+    public List<ServiceModel> getAll(){
+        return serviceRepository.findAll();
+    }
 
-	
-	public Optional<ServiceModel> getServiceById(long id) {
-		return serviceRepository.findById(id);
-	}
-	
-	public List<ServiceModel> getServicesByType(long id) throws NotFoundException{
-		Optional<TypeService> optionalType = typeServiceRepository.findById(id);
-		if(optionalType.isEmpty()) throw new NotFoundException("Type don't exist");
-		TypeService type = optionalType.get();
-		return serviceRepository.findByTypeService(type);
-	}
-	
-	public List<TypeService> getTypesService(){
-		return typeServiceRepository.findAll();
-	}
-	
-	public void createService(ServiceModel service) throws ConflictException{
-		if(serviceRepository.existsByName(service.getName())) throw new ConflictException("Service already exists.");
-		
-		int idTypeService = service.getTypeService().getId();
-		Optional<TypeService> optionalTypeService = typeServiceRepository.findById((long) idTypeService);
-		if(optionalTypeService.isEmpty()) throw new ConflictException("Invalid type service with id: " + idTypeService);
-		service.setTypeService(optionalTypeService.get());
-		
-		for(ServiceMaterial serviceMaterial: service.getServiceMaterials()) {
-			Material material = serviceMaterial.getMaterial();
-			Optional<Material> optionalMaterial = materialRepository.findById((long) material.getId());
-			if(optionalMaterial.isEmpty()) throw new ConflictException("Invalid material with id: " + material.getId());
-			material = optionalMaterial.get();
-			serviceMaterial.setMaterial(material);
-			serviceMaterial.setService(service);
-		}
-		
-		if(service.getEmployees() != null) throw new ConflictException("Cannot create a service with employees");
-		
-		serviceRepository.save(service);
-		serviceHasMaterialRepository.saveAll(service.getServiceMaterials());
-	}
+    
+    public ServiceModel getServiceById(long id) throws NotFoundException{
+        Optional<ServiceModel> optionalService = serviceRepository.findById(id);
+        if(optionalService.isEmpty()) {
+            throw new NotFoundException("Client not Found");
+        }
+        return optionalService.get();
+    }
+    
+    public List<ServiceModel> getServicesByType(long id) throws NotFoundException{
+        Optional<TypeService> optionalType = typeServiceRepository.findById(id);
+        if(optionalType.isEmpty()) throw new NotFoundException("Type don't exist");
+        TypeService type = optionalType.get();
+        return serviceRepository.findByTypeService(type);
+    }
+    
+    public List<TypeService> getTypesService(){
+        return typeServiceRepository.findAll();
+    }
+    
+    public void createService(ServiceModel service) throws ConflictException{
+        if(serviceRepository.existsByName(service.getName())) throw new ConflictException("Service already exists.");
+        
+        int idTypeService = service.getTypeService().getId();
+        Optional<TypeService> optionalTypeService = typeServiceRepository.findById((long) idTypeService);
+        if(optionalTypeService.isEmpty()) throw new ConflictException("Invalid type service with id: " + idTypeService);
+        service.setTypeService(optionalTypeService.get());
+        
+        for(ServiceMaterial serviceMaterial: service.getServiceMaterials()) {
+            Material material = serviceMaterial.getMaterial();
+            Optional<Material> optionalMaterial = materialRepository.findById((long) material.getId());
+            if(optionalMaterial.isEmpty()) throw new ConflictException("Invalid material with id: " + material.getId());
+            material = optionalMaterial.get();
+            serviceMaterial.setMaterial(material);
+            serviceMaterial.setService(service);
+        }
+        
+        if(service.getEmployees() != null) throw new ConflictException("Cannot create a service with employees");
+        
+        serviceRepository.save(service);
+        serviceHasMaterialRepository.saveAll(service.getServiceMaterials());
+    }
 }
